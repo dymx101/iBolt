89e7b970bfd679167e131211a10a92ba0fcd53d0
Daniel Dong <ydong@gagein.com>
2014-01-27 11:25:42 +0800

    add backlog

---
+++ iBolter/iBolter/Documentations/BackLog.m
@@ -0,0 +1,13 @@
+//
+//  BackLog.m
+//  iBolter
+//
+//  Created by Dong Yiming on 1/27/14.
+//  Copyright (c) 2014 Frodo. All rights reserved.
+//
+
+#import "BackLog.h"
+
+@implementation BackLog
+
+@end
