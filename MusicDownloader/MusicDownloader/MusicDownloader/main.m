9fccedfe98dbae0a6e617fff7149c4adadbc3e04
Daniel Dong <ydong@gagein.com>
2014-01-30 08:57:31 +0800

    add new project

---
+++ MusicDownloader/MusicDownloader/MusicDownloader/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MusicDownloader
+//
+//  Created by Dong Yiming on 1/30/14.
+//  Copyright (c) 2014 Frodo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FDAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FDAppDelegate class]));
+    }
+}
