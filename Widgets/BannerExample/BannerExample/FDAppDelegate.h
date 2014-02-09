de58c0556e5477aad36653618da09bd6cf08b406
Daniel Dong <ydong@gagein.com>
2014-02-09 16:37:11 +0800

    add admob demo project & incorporating the SDK

---
+++ Widgets/BannerExample/BannerExample/FDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FDAppDelegate.h
+//  BannerExample
+//
+//  Created by Dong Yiming on 2/9/14.
+//  Copyright (c) 2014 Frodo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Widgets/BannerExample/BannerExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BannerExample
+//
+//  Created by Dong Yiming on 2/9/14.
+//  Copyright (c) 2014 Frodo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FDAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FDAppDelegate class]));
+    }
+}
