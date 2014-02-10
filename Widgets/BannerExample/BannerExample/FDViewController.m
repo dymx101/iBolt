51825693d3b4635278d7f96c051943ccfc1a8a09
Daniel Dong <ydong@gagein.com>
2014-02-10 18:03:24 +0800

    iad banner

--- Widgets/BannerExample/BannerExample/FDViewController.m
+++ Widgets/BannerExample/BannerExample/FDViewController.m
@@ -10,11 +10,13 @@
 
 #import "FDViewController.h"
 #import "GADBannerView.h"
+#import <iAd/iAd.h>
 
 #define MY_BANNER_UNIT_ID @"a152f5fd1fc166f"
 
-@interface FDViewController () {
+@interface FDViewController () <ADBannerViewDelegate> {
     GADBannerView *bannerView_;
+    ADBannerView *bannerView;
 }
 
 @end
@@ -34,6 +36,8 @@
 {
     [super viewDidLoad];
 	
+#if 0
+    //admob
     bannerView_ = [[GADBannerView alloc] initWithAdSize:kGADAdSizeBanner];
     bannerView_.adUnitID = MY_BANNER_UNIT_ID;
     
@@ -43,7 +47,36 @@
     GADRequest *request = [GADRequest request];
     request.testDevices = @[GAD_SIMULATOR_ID];
     [bannerView_ loadRequest:request];
+    
+#else
+    //iad
+    bannerView = [[ADBannerView alloc]initWithFrame:
+                  CGRectMake(0, 0, 320, 50)];
+    // Optional to set background color to clear color
+    [bannerView setBackgroundColor:[UIColor clearColor]];
+    bannerView.delegate = self;
+    [self.view addSubview: bannerView];
+    
+#endif
 }
 
+
+#pragma mark - AdViewDelegates
+
+-(void)bannerView:(ADBannerView *)banner
+didFailToReceiveAdWithError:(NSError *)error{
+    NSLog(@"Error loading:%@", error);
+}
+
+-(void)bannerViewDidLoadAd:(ADBannerView *)banner{
+    NSLog(@"Ad loaded");
+}
+-(void)bannerViewWillLoadAd:(ADBannerView *)banner{
+    NSLog(@"Ad will load");
+}
+-(void)bannerViewActionDidFinish:(ADBannerView *)banner{
+    NSLog(@"Ad did finish");
+    
+}
 
 @end
