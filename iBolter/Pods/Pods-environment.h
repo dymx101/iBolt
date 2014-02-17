332c6f1e4be96a101430f407f5dc943df645b670
Daniel Dong <ydong@gagein.com>
2014-02-17 18:44:00 +0800

    add vimeo extractor

--- iBolter/Pods/Pods-environment.h
+++ iBolter/Pods/Pods-environment.h
@@ -140,7 +140,13 @@
 
 // UIAlertView-Blocks
 #define COCOAPODS_POD_AVAILABLE_UIAlertView_Blocks
-#define COCOAPODS_VERSION_MAJOR_UIAlertView_Blocks 0
+#define COCOAPODS_VERSION_MAJOR_UIAlertView_Blocks 1
 #define COCOAPODS_VERSION_MINOR_UIAlertView_Blocks 0
-#define COCOAPODS_VERSION_PATCH_UIAlertView_Blocks 1
+#define COCOAPODS_VERSION_PATCH_UIAlertView_Blocks 0
+
+// YTVimeoExtractor
+#define COCOAPODS_POD_AVAILABLE_YTVimeoExtractor
+#define COCOAPODS_VERSION_MAJOR_YTVimeoExtractor 0
+#define COCOAPODS_VERSION_MINOR_YTVimeoExtractor 0
+#define COCOAPODS_VERSION_PATCH_YTVimeoExtractor 5
 
